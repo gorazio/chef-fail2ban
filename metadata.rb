9ea6414fd3827494564fbb705c4a93ea023ea23f
gorazio <gmail@gorazio.com>
2015-03-09 17:34:37 +0300

    remove %()S from action

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        'Chef Software, Inc.'
 maintainer_email  'cookbooks@chef.io'
 license           'Apache 2.0'
 description       'Installs and configures fail2ban'
-version           '2.2.1'
+version           '2.2.2'
 
 recipe 'fail2ban', 'Installs and configures fail2ban'
 
